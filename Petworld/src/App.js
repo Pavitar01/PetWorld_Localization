20d396b10ba6ea24a3c6abf9b0ededa778cd1899
Pavitar Singh <71433170+Pavitar01@users.noreply.github.com>
2023-08-08 11:49:13 +0530

    Add files via upload

---
+++ Petworld/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Main from './pages/Main'
+import "./css/All.css"
+const App = () => {
+  return (
+    <div className='parent'>
+      <Main/>
+    </div>
+  )
+}
+
+export default App
---
+++ Petworld/src/config/firebase/Index.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+const firebaseConfig = {
+  apiKey: "AIzaSyB-ciIYzQ9W-N797b--E_4i_dDseoUxRGE",
+  authDomain: "typescript-a84d7.firebaseapp.com",
+  projectId: "typescript-a84d7",
+  storageBucket: "typescript-a84d7.appspot.com",
+  messagingSenderId: "278529870760",
+  appId: "1:278529870760:web:130eb50d538d5cccd0ab9d",
+  measurementId: "G-X327JEZVHR",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export { app, auth };
